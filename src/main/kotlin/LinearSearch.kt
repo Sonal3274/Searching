49533abb0e6ce88d766765273018c74db50969a8
Sonal <sonal.dhage26@gmail.com>
2023-05-02 21:50:54 +0200

    added a condition to indicate number not found

--- src/main/kotlin/LinearSearch.kt
+++ src/main/kotlin/LinearSearch.kt
@@ -1,10 +1,17 @@
 fun main() {
     val arr = arrayOf<Int>(10, 40, 70, 30, 60, 50, 80)
     val key = 50
+    var found = false
     for (i in arr.indices){
         if (key == arr[i]){
             println(arr[i])
+            found = true
             break
         }
+
+    }
+    if (!found) {
+        println("number not found")
     }
 }
+
