27ce8b3cb602f836c04ff73fbce9ac9ad285c34a
Sonal <sonal.dhage26@gmail.com>
2023-05-21 22:11:58 +0200

    added a condition that array must be sorted in binary search

--- src/main/kotlin/BinarySearch.kt
+++ src/main/kotlin/BinarySearch.kt
@@ -1,4 +1,8 @@
 fun binarySearch(arr: IntArray, key: Int): Int {
+    if (!arr.isSorted()) {
+        throw IllegalArgumentException("Array must be sorted for binary search.")
+    }
+
     var low = 0
     var high = arr.size - 1
 
@@ -13,15 +17,28 @@ fun binarySearch(arr: IntArray, key: Int): Int {
 
     return -1
 }
+
+fun IntArray.isSorted(): Boolean {
+    for (i in 1 until size) {
+        if (this[i] < this[i - 1]) {
+            return false
+        }
+    }
+    return true
+}
+
 fun main() {
     val arr = intArrayOf(10, 20, 30, 40, 50, 60, 70, 80, 90)
-    arr.sort() // Sort the array in ascending order
-    val key = 80
+    val key = 50
 
-    val index = binarySearch(arr, key)
-    if (index != -1) {
-        println("Element found at index $index")
+    if (arr.isSorted()) {
+        val index = binarySearch(arr, key)
+        if (index != -1) {
+            println("Element found at index $index")
+        } else {
+            println("Element not found")
+        }
     } else {
-        println("Element not found")
+        println("Array must be sorted for binary search.")
     }
 }
